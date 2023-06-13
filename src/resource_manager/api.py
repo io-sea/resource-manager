7975f844e80e81a9d8640121154ebd3e936b7aca
Jan Faltynek <fal0046@vsb.cz>
2023-06-13 12:43:33 +0200

    [Update] Logging

--- src/resource_manager/api.py
+++ src/resource_manager/api.py
@@ -2,8 +2,8 @@ import json
 from flask import Flask, jsonify
 from flask_restx import Resource, Api, reqparse
 from flask_restx import fields, marshal
-from resource_manager import resource_manager
-from logger import *
+from .resource_manager import resource_manager
+from .logger import *
 
 app = Flask('Resource Manager')
 api = Api(app)
@@ -56,11 +56,11 @@ class Allocation(Resource):
             except:
                 location = None
         except:
-            logger.info('Allocation call - Error - Arguments parser (%s)', name)
+            rm_logger.info('Allocation call - Error - Arguments parser (%s)', name)
             return {'message': 'Error - Arguments parser'}, 500
 
         if(name == None or user == None or user_slurm_token == None or es_type == None):
-            logger.info('Allocation call - Error - Missing argument (%s)', name)
+            rm_logger.info('Allocation call - Error - Missing argument (%s)', name)
             return {'message': 'Error - Missing argument'}, 500
 
         print("location:" + str(location))
@@ -71,7 +71,7 @@ class Allocation(Resource):
             if(flavor != None):
                 flavor_property = resourceManager.getFlavorProperty(flavor)
                 if(flavor_property == -1):
-                    logger.info('Allocation call - Error - Flavor {%s} does not exist (%s)', flavor, name)
+                    rm_logger.info('Allocation call - Error - Flavor {%s} does not exist (%s)', flavor, name)
                     return {'message': 'Error - Flavor does not exist'}, 500
 
                 cores = flavor_property['cores']
@@ -80,14 +80,14 @@ class Allocation(Resource):
 
             res = resourceManager.allocRequest(name, user, user_slurm_token, es_type, servers, cores, msize, ssize, targets, mountpoint, location)
             if(res == -1):
-                logger.info('Allocation call - Not enough space - added to Queue (%s)\n    Req: name:%s  user:%s  es_type:%s  servers:%s  attributes%s:', name, name, user, es_type, str(servers), str(attributes))
+                rm_logger.info('Allocation call - Not enough space - added to Queue (%s)\n       Req: name:%s  user:%s  es_type:%s  servers:%s  attributes:%s', name, name, user, es_type, str(servers), str(attributes))
                 return {'message': 'Not enough space - added to Queue'}, 404
 
-            logger.info('Allocation call - Done (%s)\n    Req: name:%s  user:%s  es_type:%s  servers:%s  attributes:%s', name, user, es_type, str(servers), str(attributes))
+            rm_logger.info('Allocation call - Done (%s)\n       Req: name:%s  user:%s  es_type:%s  servers:%s  attributes:%s', name, name, user, es_type, str(servers), str(attributes))
             return {'name': res}, 200
         except Exception as ex:
             print(str(ex))
-            logger.info('Allocation call - Error - Exp: %s (%s)', str(ex), name)
+            rm_logger.info('Allocation call - Error - Exp: %s (%s)', str(ex), name)
             return {'message': 'Error - AllocRequest'}, 500
 
 @api.route('/v2.0.0/allocation/<delete_name>')
@@ -95,9 +95,11 @@ class Delete(Resource):
     def delete(self, delete_name):
         try:
             resourceManager.deleteSession(delete_name)
+            rm_logger.info('Delete call - Done (%s)', delete_name)
             return {}, 200
         except Exception as ex:
             print(str(ex))
+            rm_logger.info('Delete call - Error - Exp: %s (%s)', str(ex), delete_name)
             return {'message': 'Error'}, 500
 
 @api.route('/v2.0.0/server/allocation/<service_name>')
@@ -106,29 +108,38 @@ class GetAllocation(Resource):
         try:
             res = resourceManager.getAssignedResource(service_name)
             if (res == -1):
+                rm_logger.info('GetAllocation call - Allocation request failed because the reservation is missing (%s)', service_name)
                 return {'message': 'Allocation request failed because the reservation is missing'}, 500
             if (res == -2):
+                rm_logger.info('GetAllocation call - Allocation request failed because the reservation is ALLOCATED (%s)', service_name)
                 return {'message': 'Allocation request failed because the reservation is ALLOCATED'}, 501
+            rm_logger.info('GetAllocation call - Done (%s)', service_name)
             return res, 200
         except Exception as ex:
             print(str(ex))
+            rm_logger.info('GetAllocation call - Error - Exp: %s (%s)', str(ex), service_name)
             return {'message': 'Error'}, 502
 
 @api.route('/v2.0.0/server/resources/<server_name>')
 class GetServerResources(Resource):
     def get(self, server_name):
-        #try:
+        try:
             if(server_name == 'all'):
                 res = resourceManager.getAllServersResource()
                 if (res == -1):
+                    rm_logger.info('GetServerResources call - Error - Server does not exist(%s)', server_name)
                     return {'message': 'Server does not exist'}, 500
+                rm_logger.info('GetServerResources call - Done (%s)', server_name)
                 return res, 200
             else:
                 res = resourceManager.getServerResource(server_name)
                 if (res == -1):
+                    rm_logger.info('GetServerResources call - Error - Server does not exist(%s)', server_name)
                     return {'message': 'Server does not exist'}, 500
+                rm_logger.info('GetServerResources call - Done (%s)', server_name)
                 return res, 200
-        #except:
+        except:
+            rm_logger.info('GetServerResources call - Error - Exp: %s (%s)', str(ex), server_name)
             return {'message': 'Error'}, 501
 
 @api.route('/v2.0.0/allocation/delete/all/yes')
--- src/resource_manager/logger.py
+++ src/resource_manager/logger.py
@@ -1,11 +1,11 @@
 import logging
 
-logger = logging.getLogger('resource_manager_logger')  
+rm_logger = logging.getLogger('resource_manager_logger')  
 
-logger.setLevel(logging.INFO)
+rm_logger.setLevel(logging.INFO)
 
 file_handler = logging.FileHandler('logfile.log')
 formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s', datefmt='%d.%m.%Y %I:%M:%S %p')
 file_handler.setFormatter(formatter)
 
-logger.addHandler(file_handler)
+rm_logger.addHandler(file_handler)
