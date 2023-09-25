b922fb4bc746ed819872e21cd6c7923f12efd29e
Jan Faltynek <fal0046@vsb.cz>
2023-09-25 11:13:10 +0200

    [Update] - Logfile path

--- src/resource_manager/logger.py
+++ src/resource_manager/logger.py
@@ -4,7 +4,7 @@ rm_logger = logging.getLogger('resource_manager_logger')
 
 rm_logger.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler('logfile.log')
+file_handler = logging.FileHandler('../../logfile.log')
 formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s', datefmt='%d.%m.%Y %I:%M:%S %p')
 file_handler.setFormatter(formatter)
 
